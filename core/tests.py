96b4e3ecffaf0578b381b4cb81ae0988984891dd
preeti <akash@bigcity.in>
2026-06-11 13:16:16 +0530

    test: add custom calculation logic test case

--- core/tests.py
+++ core/tests.py
@@ -7,3 +7,17 @@ class HealthCheckTests(SimpleTestCase):
         response = self.client.get('/api/health/')
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.json()['status'], 'healthy')
+
+
+class CalculationTests(SimpleTestCase):
+     def test_addition(self):
+        """Test that our addition math logic works perfectly."""
+        calc = Calculation()
+        result = calc.add()
+        self.assertEqual(result, 30)
+
+class Calculation():
+    a = 10
+    b = 20
+    def add(self):
+        return self.a + self.b        
